6bc7782f59e56d6b0ffe9fad68d0ff63bed28c6b
Victor Dominguez <vd071090@gmail.com>
2019-10-10 17:39:06 -0400

    added crystal adding functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+let computerNum = Math.floor(Math.random()*100 + 50);
 
 let gem1 = Math.floor(Math.random()*15 + 1);
 console.log(gem1)
@@ -19,6 +20,8 @@ let wins = 0;
 
 let loss = 0;
 
+let gemTotal = userNum + gem1 + gem2 + gem3 + egg;
+
 $("#wins").html("You won: " + wins);
 $("#loss").html("You lost: " + loss);
 
@@ -26,28 +29,73 @@ $("#loss").html("You lost: " + loss);
 
 function setup() {
     let computerNum = Math.floor(Math.random()*100 + 50);
+    console.log(computerNum)
     $("#goalNumber").html("Your gem goal: " + computerNum);
-
-
+    userNum = 0;
 }
 
 function winner() {
-    if(userNum === computerNum){
-        wins++;
-        $("#wins").html(wins);
-        setup();
-    }
+   
+    wins++;
+    $("#wins").html("You won: " + wins)
+    setup();
+
 
 }
 
 function loser() {
-    if (userNum > computerNum) {
-        loss++;
-        $("#loss").html(loss);
-        setup();
-    }
+
+    loss++;
+    $("#loss").html("You lost: " + loss);
+    setup();
+
 }
 
+
+
+$("#gem1").on("click", function(){
+    userNum = userNum + gem1;
+    $("#gemTotal").html("You collected: " + userNum);
+    if (gemTotal === computerNum){
+        winner();
+    }else if(gemTotal > computerNum){
+        loser();
+    }
+
+})
+
+$("#gem2").on("click", function(){
+    userNum = userNum + gem2;
+    $("#gemTotal").html("You collected: " + userNum);
+    if (gemTotal === computerNum){
+        winner();
+    }else if(gemTotal > computerNum){
+        loser();
+    }
+
+})
+
+$("#gem3").on("click", function(){
+    userNum = userNum + gem3;
+    $("#gemTotal").html("You collected: " + userNum);
+    if (gemTotal === computerNum){
+        winner();
+    }else if(gemTotal > computerNum){
+        loser();
+    }
+
+})
+
+$("#egg1").on("click", function(){
+    userNum = userNum + egg;
+    $("#gemTotal").html("You collected: " + userNum);
+    if (gemTotal === computerNum){
+        winner();
+    }else if(gemTotal > computerNum){
+        loser();
+    }
+
+})
 
 
 
